bb168fb0929381e7a1f025c95b1ae0bb6db5f886
Sirojbek <maqsudosirojbek77@gmail.com>
2023-02-07 22:21:29 +0500

    zero

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.json({message: 'Hello from ChatGPT!'})
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.json({bot: response.data.choices[0].text});
+  } catch (error) {
+    console.error(error + '')
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(process.env.PORT || 5000, () => console.log('AI server started'))
